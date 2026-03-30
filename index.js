58c684be4e94376bafd319ffb8bef47706cdcc65
Rounak Biswal <rounak.biswal@inovaare.com>
2026-03-31 00:58:37 +0530

    refactor: refined broadcast logic

--- index.js
+++ index.js
@@ -10,16 +10,47 @@ const io = new Server(server)
 
 app.use(express.static(path.join(__dirname, "public")))
 
+const userDB = {}
+const groupChats = {}
+
 //sockets
 io.on("connection", (socket) => {
     console.log(`new user has been connected`);
     console.log('id:', socket.id);
+
     socket.on("disconnect", (reason) => {
-        console.log(`user disconnected`);
+        let currUser = userDB[socket.id]
+        currUser && delete userDB[currUser]
+        delete userDB[socket.id]
+        currUser && console.log(`${currUser} disconnected`);
     })
+
     socket.on("client-msg", msg => {
         console.log('new message from client:', msg);
-        io.emit("server-msg", msg)
+        // groupChats[userDB[socket.id]] = msg
+        // console.log('group chats:', groupChats);
+        if (userDB[socket.id] && msg) {
+            io.emit("server-msg", {
+                "username": userDB[socket.id],
+                "msg": msg
+            })
+        }
+    })
+
+    socket.on("join", username => {
+        if (socket.id in userDB) {
+            socket.emit("join-error", "user already joined")
+            return
+        }
+        if (username in userDB) {
+            let oldSocketId = userDB[username]
+            delete userDB[oldSocketId]
+        }
+        userDB[username] = socket.id
+        userDB[socket.id] = username
+        console.log("users : ", userDB);
+
+        socket.emit("join-success", "user joined !!");
     })
 })
 
